b5a34b95f35ae5d821a56c09c0c4d88e825ea6ad
abbas <abbastouqeer399@gmail.com>
2024-05-16 22:59:49 +0500

    added print.py

---
+++ print.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello world")
+
+
+# # string for number
+
+# In[2]:
+
+
+name=150
+
+
+# In[ ]:
+
+
+
+
+
+# In[3]:
+
+
+print(name)
+
+
+# In[4]:
+
+
+name+50
+
+
+# In[5]:
+
+
+a=10
+b=30
+
+
+# In[6]:
+
+
+a+b
+
+
+# In[7]:
+
+
+print(a+b)
+
+
+# In[8]:
+
+
+a="23"
+b="30"
+
+
+# In[9]:
+
+
+print(2+2)
+
+
+# In[10]:
+
+
+a=2+2
+
+
+# In[11]:
+
+
+a
+
+
+# In[12]:
+
+
+a=12/3
+
+
+# In[13]:
+
+
+a
+
+
+# In[14]:
+
+
+num1=10
+num2=1.5
+sum=num1+num2
+
+
+# In[15]:
+
+
+sum
+
+
+# In[16]:
+
+
+num1=10
+num2=5
+
+
+# In[17]:
+
+
+name=10
+
+
+# In[18]:
+
+
+name
+
+
+# In[19]:
+
+
+ROSE=10
+
+
+# In[20]:
+
+
+ROSE
+
+
+# In[1]:
+
+
+name = int(input("Enter the number: "))
+
+
+# In[2]:
+
+
+name
+
+
+# In[ ]:
+
+
+
+
